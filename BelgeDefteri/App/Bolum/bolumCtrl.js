eca2154cf8bf8c74bb8b08e57c49db9cfa302690
fatih <fsahinbas@gmail.com>
2016-07-29 13:30:11 +0300

    getAll for kurs

--- BelgeDefteri/App/Bolum/bolumCtrl.js
+++ BelgeDefteri/App/Bolum/bolumCtrl.js
@@ -1,4 +1,10 @@
 ﻿app.controller("bolumCtrl", ['$scope', '$http',
 function ($scope,$http) {
     $scope.mesaj = "Bölüm çalışıyor...";
+    $http.get("../Api/Kurs/getAll").then(function (response) {
+        $scope.myData = response.data;
+    });
 }]);
+
+
+    //~/Api/Kurs/getAll
