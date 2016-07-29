77a1ddfb45d60c9b251cdf1f23f24c104e318288
fatih <fsahinbas@gmail.com>
2016-07-29 15:54:22 +0300

    kayit ekleme ve listeleme denemesi

--- BelgeDefteri/App/Bolum/bolumCtrl.js
+++ BelgeDefteri/App/Bolum/bolumCtrl.js
@@ -1,16 +1,18 @@
 ﻿app.controller("bolumCtrl", ['$scope', '$http',
-function ($scope,$http) {
+function ($scope, $http) {
     $scope.mesaj = "Bölüm çalışıyor...";
 
     $http.get("../Api/Kurs/getAll").then(function (response) {
         $scope.myData = response.data;
 
-     
     });
 
-  
-
+    $scope.save = function () {
+        $http.post("../Api/Kurs/Insert", $scope.Model).then(function (item) {
+            $scope.Model = item.data;
+        })
+    };
 }]);
 
 
-    //~/Api/Kurs/getAll
+//~/Api/Kurs/getAll
